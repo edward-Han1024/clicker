f43a6634085b01cd4672a9d6507dd65fdc21f523
edward-Han1024 <babyed.han@gmail.com>
2024-08-27 20:48:14 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024 by Edward Han in 2024 by the CC BY SA License
+// constants:
+Game = {};
+Game.fps = 30;
+// main code:
+Game.Update = function(){};
+// start game 
+mainloop = setInterval(Game.Update(), 1000/Game.fps);
